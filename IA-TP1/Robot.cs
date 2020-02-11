7ed6883f1d674f789d1fd1d8911cc40b79631a49
Ilias H <haouchilias@hotmail.com>
2020-02-10 21:56:24 -0400

    add AStarSearch

--- IA-TP1/Robot.cs
+++ IA-TP1/Robot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,9 @@ namespace IA_TP1
     {
         public string actions;
         public int[] pos;
+        public List<int[]> dirtLeft;
+        public List<int[]> bijouLeft;
+        public int heuristic;
     }
 
     class Robot
@@ -36,7 +40,8 @@ namespace IA_TP1
             while (true)
             {
                 memoire = c.captureEnv();
-                actionsList = search();
+                actionsList = AStarSearch();
+                //actionsList = search();
                 execActions(actionsList);
             }
         }
@@ -45,6 +50,139 @@ namespace IA_TP1
 
         public room[,] Memoire { get => memoire; set => memoire = value; }
         public int[] Position { get => position; set => position = value; }
+
+        public int compare2Nodes(noeud n2, noeud n1)
+        {
+            if (n1.heuristic == n2.heuristic)
+            {
+                return 0;
+            }
+            else if (n1.heuristic < n2.heuristic)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public List<int[]> FindAllDirt(room[,] memoire)
+        {
+            List<int[]> res = new List<int[]>();
+            for (int i = 0; i < memoire.GetLength(0); i++)
+            {
+                for (int j = 0; j < memoire.GetLength(1); j++)
+                {
+                    if (memoire[i, j].hasDirt)
+                    {
+                        int[] posi = { i, j };
+                        res.Add(posi);
+                    }
+                }
+            }
+            return res;
+        }
+
+        public List<int[]> FindAllBijou(room[,] memoire)
+        {
+            List<int[]> res = new List<int[]>();
+            for (int i = 0; i < memoire.GetLength(0); i++)
+            {
+                for (int j = 0; j < memoire.GetLength(1); j++)
+                {
+                    if (memoire[i, j].hasBijoux)
+                    {
+                        int[] posi = { i, j };
+                        res.Add(posi);
+                    }
+                }
+            }
+            return res;
+        }
+
+        public int findHeuristic(noeud n)
+        {
+            return (n.bijouLeft.Count + n.dirtLeft.Count);
+        }
+
+
+        public string AStarSearch()
+        {
+            List<noeud> graph = new List<noeud>();
+            noeud n = new noeud();
+            n.actions = "";
+            n.pos = position;
+            n.bijouLeft = FindAllBijou(memoire);
+            n.dirtLeft = FindAllDirt(memoire);
+            n.heuristic = findHeuristic(n);
+
+            graph.Add(n);
+            bool solutionFound = testSolution(graph[0].actions);
+            while (!solutionFound)
+            {
+                string actionspossible = getAllPossibleActions(graph[0].pos);
+                foreach (char action in actionspossible)
+                {
+                    noeud node = new noeud();
+                    node.actions = "";
+                    node.pos = new int[2];
+                    Array.Copy(graph[0].pos, node.pos, graph[0].pos.Length);
+                    node.heuristic = graph[0].heuristic;
+                    node.bijouLeft = new List<int[]>(graph[0].bijouLeft);
+                    node.dirtLeft = new List<int[]>(graph[0].dirtLeft);
+                    node.actions = graph[0].actions + action;
+                    switch (action)
+                    {
+                        case 'h':
+                            node.pos[0] -= 1;
+                            break;
+                        case 'b':
+                            node.pos[0] += 1;
+                            break;
+                        case 'g':
+                            node.pos[1] -= 1;
+                            break;
+                        case 'd':
+                            node.pos[1] += 1;
+                            break;
+                        case 'a':
+                            if (node.dirtLeft.Contains(node.pos))
+                            {
+                                node.dirtLeft.Remove(node.pos);
+                                node.heuristic = findHeuristic(node);
+                            }
+                            break;
+                        case 'r':
+                            if (node.bijouLeft.Contains(node.pos))
+                            {
+                                node.bijouLeft.Remove(node.pos);
+                                node.heuristic = findHeuristic(node);
+                            }
+                            break;
+                    }
+                    for (int i = 0; i < graph.Count; i++)
+                    {
+                        if (i >= graph.Count - 1)
+                        {
+                            graph.Add(node);
+                            break;
+                        }
+                        if (graph[i].heuristic > node.heuristic)
+                        {
+                            if (i == 0) { ++i; }
+                            graph.Insert(i, node);
+                            break;
+                        }
+                    }
+                }
+                graph.RemoveAt(0);
+                //graph.Sort(compare2Nodes);
+                solutionFound = testSolution(graph[0].actions);
+            }
+            return (graph[0].actions);
+        }
+
 
         public string search()
         {
